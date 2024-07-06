86ae4eff2bbaee54c3c21dade191da3507fe1353
suhan8984 <suhan8984@daum.net>
2024-07-06 17:22:22 +0900

    feat: HomePage without footer
    footer 제외하고 홈 디자인 했어요.

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,11 +1,12 @@
+import Banner from "../components/Banner";
 // 홈화면
 const HomePage = () => {
   return (
     <div>
-      {/* posting1 */}
-      {/* posting2 */}
-      {/* posting3 */}
-      {/* posting4 */}
+      <Banner key={0} id={0} />
+      <Banner key={1} id={1} />
+      <Banner key={2} id={2} />
+      <Banner key={3} id={3} />
     </div>
   );
 };
