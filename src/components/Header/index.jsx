81c8bbe4cf8df3e6614f6157b81590d1b07e66fe
suhan8984 <suhan8984@daum.net>
2024-07-06 21:27:23 +0900

    add: github links, reload window for logo
    footer에 링크 추가
    로고 클릭 시 화면 reload

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,13 +5,13 @@ import logo from "../../assets/images/title.png";
 const Header = () => {
   return (
     <div className="fixed z-50 flex w-[1440px] h-[100px] items-center gap-[180px] px-[110px] py-[30px] bg-white">
-      <Link to="/">
+      <a href="/">
         <img
           className="relative w-[226px] h-[53px] mt-[-2.00px] mb-[-2.00px] object-cover"
           alt="Element"
           src={logo}
         />
-      </Link>
+      </a>
       <div className="inline-flex items-center gap-[120px] relative flex-[0_0_auto]">
         <Link to="/">
           <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Regular',Helvetica] font-normal text-[#3f3f3f] text-[16px] text-sm tracking-[0] leading-[normal]">
