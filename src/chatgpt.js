4dfb5cd71d3ec5bcfcedb0ef2b9334d25bf5f407
jsh6269 <55016623+jsh6269@users.noreply.github.com>
2025-02-24 19:59:37 +0900

    Update chatgpt.js

--- src/chatgpt.js
+++ src/chatgpt.js
@@ -8,7 +8,7 @@ const openai = new OpenAI({
 });
 
 async function LabelSearchAPI(base64Image) {
-  const response = await fetch("http://localhost:5000/predict", {
+  const response = await fetch("https://carewise-server.onrender.com/predict", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
